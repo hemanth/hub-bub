72394e6ed68a9bb18a9fbb710ba4ddcd16cef172
Hemanth.HM <hemanth.hm@gmail.com>
2015-11-11 17:34:41 +0530

    More paths

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -3,15 +3,6 @@ chrome.extension.sendMessage({}, function(response) {
 	if (document.readyState === "complete") {
 		clearInterval(readyStateCheckInterval);
 
-    $('<input />')
-    .attr({
-      id: 'search',
-      autocomplete: 'off',
-      placeholder: 'Search your repo..',
-      style: 'margin-bottom: 10px;width: 100%; font-weight: bolder;'
-    })
-    .prependTo('.filter-list.small')
-
     var issueList = $('.filter-list.small');
     
     issueList.children().detach().sort(function(a, b) {
@@ -19,13 +10,22 @@ chrome.extension.sendMessage({}, function(response) {
       .localeCompare($(b).find('a').attr('title'));
     }).appendTo(issueList);
 
-		$(function() {
+    $('<input />')
+    .attr({
+      id: 'search',
+      autocomplete: 'off',
+      placeholder: 'Search your repo..',
+      style: 'margin-bottom: 10px;width: 100%; font-weight: bolder;'
+    })
+    .prependTo('.filter-list.small');
+    
+    $(function() {
       $('input#search').keyup(function() {
         var matches = $( 'ul.filter-list.small' ).find('li:contains('+ $( this ).val() +') ');
         $('li', 'ul.filter-list.small').not(matches).slideUp();
         matches.slideDown();
       });
-		});
+    });
 	}
 	}, 10);
 });
