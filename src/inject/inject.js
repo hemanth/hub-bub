0d31c590d5df873d088a9564592f19e77d7a7c94
Hemanth.HM <hemanth.hm@gmail.com>
2015-11-11 16:28:59 +0530

    Better sort and fixes #1

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -3,37 +3,28 @@ chrome.extension.sendMessage({}, function(response) {
 	if (document.readyState === "complete") {
 		clearInterval(readyStateCheckInterval);
 
-		$('<input />')
-		.attr({
-		  id: 'search',
-		  autocomplete: 'off',
-		  style: 'margin-bottom: 10px;width: 100%; font-weight: bolder;'
-		})
-		.prependTo('.filter-list.small')
+    $('<input />')
+    .attr({
+      id: 'search',
+      autocomplete: 'off',
+      placeholder: 'Search your repo..',
+      style: 'margin-bottom: 10px;width: 100%; font-weight: bolder;'
+    })
+    .prependTo('.filter-list.small')
 
-		var items = $('.filter-list.small li').get();
+    var issueList = $('.filter-list.small');
+    
+    issueList.children().detach().sort(function(a, b) {
+      return $(a).find('a').attr('title')
+      .localeCompare($(b).find('a').attr('title'));
+    }).appendTo(issueList);
 
-		items.sort(function(a,b){
-		  var keyA = $(a).text();
-		  var keyB = $(b).text();
-
-		  if (keyA < keyB) return -1;
-		  if (keyA > keyB) return 1;
-		  return 0;
-		});
-
-		var ul = $('.filter-list.small');
-
-		$.each(items, function(i, li){
-		  ul.append(li);
-		});
-
-		$( function() {
-		    $('input#search').keyup( function() {
-		        var matches = $( 'ul.filter-list.small' ).find('li:contains('+ $( this ).val() +') ');
-		        $('li', 'ul.filter-list.small').not(matches).slideUp();
-		        matches.slideDown();
-		    });
+		$(function() {
+      $('input#search').keyup(function() {
+        var matches = $( 'ul.filter-list.small' ).find('li:contains('+ $( this ).val() +') ');
+        $('li', 'ul.filter-list.small').not(matches).slideUp();
+        matches.slideDown();
+      });
 		});
 	}
 	}, 10);
